76ab6bd0bf5d2cebbf30874320987001f29727ed
mghoriresearch <alighori@gmail.com>
2016-07-04 11:48:44 -0500

    Not branch comit

---
+++ MAIN/mastercode.m
@@ -0,0 +1,31 @@
+%% FULL WINDED TITLE 
+%%                      Author: Mohamed K. Ghori B.S. M.E.
+%                      ------------------------------------
+% 
+% Acknowledgements:
+%
+
+
+function [ output_args, Task ] = Untitled2( input_args )
+%Print Task Name
+Task = 'WHAT TASK IS RUNNING';
+%---------------------
+% INSTRUCTIONS
+%{
+% PUT INFO ON HOW TO USE FUNCTION
+%}
+
+% Enough Inputs EXCEPTION
+if nargin == 0
+%% SECTION 1
+
+% LINE COMMENT 
+
+%% SECTION 2
+...
+
+else
+    ERROR = 'Not Enough Input Arguments'
+end
+end
+
